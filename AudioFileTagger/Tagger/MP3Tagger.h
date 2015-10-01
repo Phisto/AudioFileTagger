3bb335634d1813d511fa38ed7e2fd694e0691a55
phisto05 <phisto05@gmail.com>
2015-10-01 12:54:23 +0200

    initial commit

---
+++ AudioFileTagger/Tagger/MP3Tagger.h
@@ -0,0 +1,13 @@
+//
+//  MP3Tagger.h
+//  AudioFileTagger
+//
+//  Created by Simon Gaus on 01.10.15.
+//  Copyright © 2015 Simon Gaus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MP3Tagger : NSObject
+
+@end
---
+++ AudioFileTagger/Tagger/MP3Tagger.m
@@ -0,0 +1,13 @@
+//
+//  MP3Tagger.m
+//  AudioFileTagger
+//
+//  Created by Simon Gaus on 01.10.15.
+//  Copyright © 2015 Simon Gaus. All rights reserved.
+//
+
+#import "MP3Tagger.h"
+
+@implementation MP3Tagger
+
+@end
